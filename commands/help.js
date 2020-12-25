da607231ee7a8d6e930b152e9d4b0cc6c73c6390
KevinCTran <kebintrann@gmail.com>
2020-12-24 23:07:46 -0800

    help msg

--- commands/help.js
+++ commands/help.js
@@ -5,14 +5,11 @@ module.exports = {
     description: 'Bot commands',
     args: false,
 	execute(message, args, ouBets, vsBets) {    
-        message.channel.send(`\`\`\` Very basic commands. Input is not thoroughly checked. 
-        **ALWAYS** keep players/teams to one word.\n Current supported commands: \n
-        \`- !bets : Lists all bets \` 
-        \`- !help : This message lmao
-        \`- !o/u <Player/Team> <Number> <Cat> : Creates an over/under bet 
-        \`- !vs <Player/Team> <Player/Team> <Cat> : Creates a versus bet.
-
-        React to messages to place your bet. Every bet is considered $1 for now until we aint broke.
+        message.channel.send(`\`\`\` Very basic commands. Input is not thoroughly checked.\n **ALWAYS** keep players/teams to one word.\n Current supported commands: \n
+        - !bets : Lists all bets 
+        - !help : This message lmao
+        - !o/u <Player/Team> <Number> <Cat> : Creates an over/under bet 
+        - !vs <Player/Team> <Player/Team> <Cat> : Creates a versus bet.\n\nReact to messages to place your bet. Every bet is considered $1 for now until we aint broke.
         \`\`\``);
 	},
 };
