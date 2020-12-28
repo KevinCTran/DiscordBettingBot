f1a5690c65ec4a108e39e14a27ea305c0284a020
KevinCTran <kebintrann@gmail.com>
2020-12-27 22:31:01 -0800

    help message

--- commands/help.js
+++ commands/help.js
@@ -5,10 +5,12 @@ module.exports = {
 	execute(message, args, ouBets, vsBets, messageIds, winnerMap) {    
         message.channel.send(`\`\`\` Very basic commands. Input is not thoroughly checked.\n **ALWAYS** keep players/teams to one word.\n Current supported commands: \n
         - !bets : Lists all bets 
-        - !help : This message lmao
+        - !help : This message
         - !o/u <Player/Team> <Number> <Cat> : Creates an over/under bet 
         - !vs <Player/Team> <Player/Team> <Cat> : Creates a versus bet.
-        - !remove <betType (o/u, vs)> <Same params as o/u or vs>\n\nReact to messages in the correct channel to place your bet. Every bet is considered $1 for now until we aint broke.
+        - !remove <betType (o/u, vs)> <Same params as o/u or vs>
+        - !setWinners : Lists all valid bets in #set-winners channel so we can react and set which players/teams won
+        - !winners : Calculates and displays who owes who money\n\nReact to messages in the correct channel to place your bet. Every bet is considered $1 for now until we aint broke.
         \`\`\``);
 	},
 };
