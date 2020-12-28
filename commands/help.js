638984894d28a5290520f80d31f816baab1ba3c0
KevinCTran <kebintrann@gmail.com>
2020-12-28 02:08:42 -0800

    help msg change

--- commands/help.js
+++ commands/help.js
@@ -9,7 +9,7 @@ module.exports = {
         - !o/u <Player/Team> <Number> <Cat> : Creates an over/under bet 
         - !vs <Player/Team> <Player/Team> <Cat> : Creates a versus bet.
         - !remove <betType (o/u, vs)> <Same params as o/u or vs>
-        - !setWinners : Lists all valid bets in #set-winners channel so we can react and set which players/teams won
+        - !setWinners : Lists bets in #set-winners for reactions to set winners
         - !winners : Calculates and displays who owes who money\n\nReact to messages in the correct channel to place your bet. Every bet is considered $1 for now until we aint broke.
         \`\`\``);
 	},
