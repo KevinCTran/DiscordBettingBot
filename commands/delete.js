63614270ff66780f4e147bbcfcc8bc0a82aebc01
KevinCTran <kebintrann@gmail.com>
2020-12-30 23:07:45 -0800

    delete

--- commands/delete.js
+++ commands/delete.js
@@ -6,57 +6,56 @@ module.exports = {
     description: 'Delete all bets',
     args: false,
 	async execute(message, args, ouBets, vsBets, messageIds, winnerMap, dbClient) {    
-        if (message.author.id === '325531540547436545') {
-            ouBets.clear();
-            vsBets.clear();
-            messageIds.clear();
-            winnerMap.clear();
-            console.log("Cleared the Maps");
+        ouBets.clear();
+        vsBets.clear();
+        messageIds.clear();
+        winnerMap.clear();
+        console.log("Cleared the Maps");
 
-            // Clear the messages from all text channels
+        // Clear the messages from all text channels
 
-            // Over/Under
-            await message.client.channels.cache.get(process.env.OU_CHANNEL).messages.fetch({limit: 100}).then(messages => {
-                message.client.channels.cache.get(process.env.OU_CHANNEL).bulkDelete(messages). then(() => {
-                    message.client.channels.cache.get(process.env.OU_CHANNEL).send(`Cleared messages!`).then(msg => {
-                        msg.delete({timeout: 3000});
-                    });
+        // Over/Under
+        await message.client.channels.cache.get(process.env.OU_CHANNEL).messages.fetch({limit: 100}).then(messages => {
+            message.client.channels.cache.get(process.env.OU_CHANNEL).bulkDelete(messages). then(() => {
+                message.client.channels.cache.get(process.env.OU_CHANNEL).send(`Cleared messages!`).then(msg => {
+                    msg.delete({timeout: 3000});
                 });
-            }).catch((err) => {
-                console.log(err);
-                return message.reply("An error occured");
             });
+        }).catch((err) => {
+            console.log(err);
+            return message.reply("An error occured");
+        });
 
-            // VS 
-            await message.client.channels.cache.get(process.env.VS_CHANNEL).messages.fetch({limit: 100}).then(messages => {
-                message.client.channels.cache.get(process.env.VS_CHANNEL).bulkDelete(messages). then(() => {
-                    message.client.channels.cache.get(process.env.VS_CHANNEL).send(`Cleared messages!`).then(msg => {
-                        msg.delete({timeout: 3000});
-                    });
+        // VS 
+        await message.client.channels.cache.get(process.env.VS_CHANNEL).messages.fetch({limit: 100}).then(messages => {
+            message.client.channels.cache.get(process.env.VS_CHANNEL).bulkDelete(messages). then(() => {
+                message.client.channels.cache.get(process.env.VS_CHANNEL).send(`Cleared messages!`).then(msg => {
+                    msg.delete({timeout: 3000});
                 });
-            }).catch((err) => {
-                console.log(err);
-                return message.reply("An error occured");
             });
+        }).catch((err) => {
+            console.log(err);
+            return message.reply("An error occured");
+        });
 
-            // Set Winners
-            await message.client.channels.cache.get(process.env.WIN_CHANNEL).messages.fetch({limit: 100}).then(messages => {
-                message.client.channels.cache.get(process.env.WIN_CHANNEL).bulkDelete(messages). then(() => {
-                    message.client.channels.cache.get(process.env.WIN_CHANNEL).send(`Cleared messages!`).then(msg => {
-                        msg.delete({timeout: 3000});
-                    });
+        // Set Winners
+        await message.client.channels.cache.get(process.env.WIN_CHANNEL).messages.fetch({limit: 100}).then(messages => {
+            message.client.channels.cache.get(process.env.WIN_CHANNEL).bulkDelete(messages). then(() => {
+                message.client.channels.cache.get(process.env.WIN_CHANNEL).send(`Cleared messages!`).then(msg => {
+                    msg.delete({timeout: 3000});
                 });
-            }).catch((err) => {
-                console.log(err);
-                return message.reply("An error occured");
             });
+        }).catch((err) => {
+            console.log(err);
+            return message.reply("An error occured");
+        });
 
-            // Separate old logs
-            await message.client.channels.cache.get(process.env.LOG_CHANNEL).send(`====================================================`).then(() => {
-                message.client.channels.cache.get(process.env.LOG_CHANNEL).send(`====================================================`);
-            });
+        // Separate old logs
+        await message.client.channels.cache.get(process.env.LOG_CHANNEL).send(`====================================================`).then(() => {
+            message.client.channels.cache.get(process.env.LOG_CHANNEL).send(`====================================================`);
+        });
 
-            message.channel.send("Deleted all existing bets");
-        }
+        message.channel.send("Deleted all existing bets");
+        
 	},
 };
--- commands/leaderboard.js
+++ commands/leaderboard.js
@@ -9,24 +9,18 @@ module.exports = {
 	execute(message, args, ouBets, vsBets, messageIds, winnerMap, dbClient) {
         const query = `select * from winnings`;
             let board = [];
-            let longestNameLen = 0;
 
             dbClient
                 .query(query)
                 .then(res => {
                     for (i = 0; i < res.rows.length; i++) {
                         board.push(res.rows[i]);
-
-                        if (res.rows[i].name.length > longestNameLen) {
-                            longestNameLen = res.rows[i].name.length;
-                        }
                     }
                     
                     board.sort(function(a, b) {
                         return b.amount - a.amount;
                     });
 
-                    let line = new Array(longestNameLen + 1).join('-');
                     const Embed = new Discord.MessageEmbed()
                         .setColor('#1e8725')
                         .setTitle('LEADERBOARD');
